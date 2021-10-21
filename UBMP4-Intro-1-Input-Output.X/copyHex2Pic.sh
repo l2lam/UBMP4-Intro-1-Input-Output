a75d13782d0344bca3c99ffd4b6df894aa58adb6
Ly Lam <lylam99@gmail.com>
2021-10-21 11:43:41 -0400

    Use relative path for source

--- UBMP4-Intro-1-Input-Output.X/copyHex2Pic.sh
+++ UBMP4-Intro-1-Input-Output.X/copyHex2Pic.sh
@@ -1,1 +1,1 @@
-cp /home/ly/src/UBMP4-Intro-1-Input-Output/UBMP4-Intro-1-Input-Output.X/dist/free/production/UBMP4-Intro-1-Input-Output.X.production.hex /media/ly/PIC16F1459/
+cp ./dist/free/production/UBMP4-Intro-1-Input-Output.X.production.hex /media/ly/PIC16F1459/
